823b8b0570a5517ce97656ead9493ed32081d8b1
Patrick Dwyer <patrick@patrickdwyer.com>
2023-11-18 16:59:43 -0600

    cv update

--- js/script.js
+++ js/script.js
@@ -11,3 +11,23 @@ detailsCoursework.forEach((details) => {
 		detailsOpen = !detailsOpen;
 	});
 });
+
+// let timeout = false;
+// const delay = 250;
+
+// introImg = document.querySelector("div.intro img");
+// introP = document.querySelector("div.intro div");
+// function updateIntroHeight() {
+// 	h1 = introImg.offsetHeight;
+// 	h2 = introP.offsetHeight;
+// 	console.log(h1);
+// 	console.log(h2);
+// 	newHeight = h1 > h2 ? h1 : h2;
+// 	console.log(newHeight);
+// 	introP.height = `${parseInt(newHeight)}px`;
+// }
+// onresize = (event) => {
+// 	// if (window.innerWidth > 650) return;
+// 	clearTimeout(timeout);
+// 	timeout = setTimeout(updateIntroHeight, delay);
+// };
