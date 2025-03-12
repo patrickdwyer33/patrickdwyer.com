2668bc8e1be00cc1029de3eafc3a8fd1bfac55a2
Patrick Dwyer <patrick@patrickdwyer.com>
2025-03-12 18:35:35 -0500

    working on vite

---
+++ src/main.js
@@ -0,0 +1,45 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import appLogo from '/favicon.svg'
+import { setupCounter } from './counter.js'
+import { initPWA } from './pwa.js'
+
+const app = document.querySelector('#app')
+app.innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${appLogo}" class="logo" alt="patrickdwyer.com logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>patrickdwyer.com</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+  <div
+    id="pwa-toast"
+    role="alert"
+    aria-labelledby="toast-message"
+  >
+    <div class="message">
+      <span id="toast-message"></span>
+    </div>
+    <div class="buttons">
+      <button id="pwa-refresh" type="button">
+        Reload
+      </button>
+      <button id="pwa-close" type="button">
+        Close
+      </button>
+    </div>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
+
+initPWA(app)
---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'patrickdwyer.com',
+      short_name: 'patrickdwyer.com',
+      description: 'Personal resume website for Patrick Dwyer',
+      theme_color: '#D3D3FF',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
