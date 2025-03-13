f85abb19ed3cf25d54e9e2eae2c979f737da6361
Patrick Dwyer <patrick@patrickdwyer.com>
2025-03-13 01:52:09 -0500

    saving | still having fonts issue

--- vite.config.js
+++ vite.config.js
@@ -20,38 +20,15 @@ export default defineConfig({
 				theme_color: "#D3D3FF",
 			},
 
-			// specifies anything to include in cache that isn't linked in index.html somehow
-			// we want to include all assets in public folder
-			includeAssets: ["**/*"],
-
 			workbox: {
 				globPatterns: ["**/*.{js,css,html,svg,png,ico,webp,pdf,woff2}"],
 				cleanupOutdatedCaches: true,
 				clientsClaim: true,
 				skipWaiting: true,
-				runtimeCaching: [
-					{
-						urlPattern: ({ url }) =>
-							url.origin === "https://patrickdwyer.com",
-						handler: "StaleWhileRevalidate",
-						options: {
-							cacheName: "media",
-							expiration: {
-								maxEntries: 500,
-								maxAgeSeconds: 60 * 60 * 24 * 365 * 2, // 2 years
-							},
-							cacheableResponse: {
-								statuses: [200],
-							},
-							rangeRequests: true,
-						},
-					},
-				],
 			},
 
 			devOptions: {
 				enabled: true,
-				navigateFallback: "index.html",
 				suppressWarnings: false,
 				type: "module",
 			},
